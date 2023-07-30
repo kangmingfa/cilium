69e0e427b2e5eac98c0918c193edb8929ae58790
kangmingfa <kangmingfa>
2023-07-31 01:40:21 +0800

    sync

--- images/cilium/download-hubble.sh
+++ images/cilium/download-hubble.sh
@@ -16,7 +16,8 @@ declare -A hubble_sha256
 hubble_sha256[amd64]="2d2768574351a85770088c9239214cf50cb42461979f116ef20f985eb943b05a"
 # renovate: datasource=github-release-attachments depName=cilium/hubble digestVersion=v0.11.6
 hubble_sha256[arm64]="6c74e702638533ae835110be59d303ee74d4a19498bdb83ad488025684276ffe"
-
+export https_proxy=192.168.7.116:7890
+export http_proxy=192.168.7.116:7890
 for arch in amd64 arm64 ; do
   curl --fail --show-error --silent --location "https://github.com/cilium/hubble/releases/download/${hubble_version}/hubble-linux-${arch}.tar.gz" --output "/tmp/hubble-${arch}.tgz"
   printf "%s %s" "${hubble_sha256[${arch}]}" "/tmp/hubble-${arch}.tgz" | sha256sum -c
--- images/runtime/build-gops.sh
+++ images/runtime/build-gops.sh
@@ -20,7 +20,8 @@ cd gops
 git checkout -b "${gops_version}" "${gops_version}"
 git --no-pager remote -v
 git --no-pager log -1
-
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
 for arch in amd64 arm64 ; do
   mkdir -p "/out/linux/${arch}/bin"
   GOARCH="${arch}" CGO_ENABLED=0 go build -ldflags "-s -w" -o "/out/linux/${arch}/bin/gops" github.com/google/gops
